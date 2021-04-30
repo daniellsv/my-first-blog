0c7cf5fd9d75675216fbd30a1eab6fd8dd4e4979
daniel  florez <dflorez@lsv-tech.com>
2021-04-30 18:29:12 -0500

    ultimos cambios de error

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .form import PostForm
 from django.shortcuts import redirect
-
 # Create your views here.
 
 
